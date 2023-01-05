c4fb01665baccc8ff41994a94d4db32f8b014d74
Afonso Costa <101286472+userunaavailable@users.noreply.github.com>
2023-01-05 19:53:21 +0100

    hash commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require ("mongoose");
-const encrypt = require('mongoose-encryption');
+const md5 = require ('md5')
 
 
 const app = express();
@@ -19,11 +19,6 @@ const userSchema = new mongoose.Schema({
     email:String,
     password:String
 });
-
-//encriptação
-//importante adicionar isto antes de criar o model
-
-userSchema.plugin(encrypt, {secret: process.env.SECRET , encryptedFields:['password']});
 
 const User = new mongoose.model("User",userSchema);
 
@@ -42,7 +37,7 @@ app.get("/register", function(req,res){
 app.post("/register",function(req,res){
     const newUser = new User ({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
     newUser.save(function(err){
         if(err){
@@ -55,7 +50,7 @@ app.post("/register",function(req,res){
 
 app.post ("/login",function(req,res){
 const username = req.body.username;
-const password = req.body.password;
+const password = md5(req.body.password);
 
 User.findOne({email:username},function(err, foundUser){
     if(err){
