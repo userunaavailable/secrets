38f86f68ea01a3a3e47df41ff7ab0d538b45d0d1
Afonso Costa <101286472+userunaavailable@users.noreply.github.com>
2023-01-08 20:13:34 +0100

    Final Commit- Google, Facebook auth and secrets submit

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const session = require("express-session");
 const passport = require ("passport");
 const passportLocalMongoose = require ("passport-local-mongoose");
 const LocalStrategy = require('passport-local');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
+const FacebookStrategy = require("passport-facebook").Strategy;
+
 
 
 
@@ -35,11 +39,17 @@ mongoose.connect('mongodb://127.0.0.1:27017/userDB', {useNewUrlParser: true});
 
 const userSchema = new mongoose.Schema({
     email:String,
-    password:String
+    password:String,
+    googleId: String,
+    facebookId: String,
+    secret: String
+
 });
 
-//passport package conecta ao Mongo
+//passport package plugin para conectar ao Mongo
 userSchema.plugin(passportLocalMongoose);
+//mongoose plugin para utilizar findorcreate
+userSchema.plugin(findOrCreate);
 
 
 const User = new mongoose.model("User",userSchema);
@@ -48,13 +58,68 @@ const User = new mongoose.model("User",userSchema);
 passport.use(new LocalStrategy(User.authenticate()));
 
 // use static serialize and deserialize of model for passport session support
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+    done(null, user);
+  });
+  
+  passport.deserializeUser(function(user, done) {
+    done(null, user);
+  });
+// google authorization
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets"
 
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+//facebook authorization  
+
+passport.use(new FacebookStrategy({
+    clientID: process.env.FB_ID,
+    clientSecret: process.env.FB_SECRET,
+    callbackURL: "http://localhost:3000/auth/facebook/secrets"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 app.get("/", function(req,res){
     res.render("home");
 });
+
+app.get("/auth/google",
+  passport.authenticate('google', { scope: ['profile'] }));
+
+
+  app.get("/auth/google/secrets", 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
+  });
+
+  app.get('/auth/facebook',
+  passport.authenticate('facebook'));
+
+app.get('/auth/facebook/secrets',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
+  });
+
 
 app.get("/login", function(req,res){
     res.render("login");
@@ -65,11 +130,41 @@ app.get("/register", function(req,res){
 });
 
 app.get("/secrets", function(req, res){
+    User.find({"secret": {$ne:null}},function(err, foundUsers){
+        if (err){
+            console.log(err);
+        }else{
+            res.render("secrets", {usersWithSecrets: foundUsers})
+        }
+    });
+});
+
+app.get("/submit", function(req,res){
     if(req.isAuthenticated()){
-        res.render("secrets");
+        res.render("submit");
     }else{
         res.redirect("/login");
     }
+});
+
+app.post("/submit", function(req,res){
+    const submittedSecret = req.body.secret;
+    // because mongoose returns the id as an object (ObjectId) and you can't extract it like that. Try to use req.user.id.toString(
+    
+    User.findById(req.user._id.toString(), function(err, foundUser){
+        if (err){
+            console.log(err);
+        }else{
+            if (foundUser){
+                foundUser.secret=submittedSecret;
+                foundUser.save(function(){
+                    res.redirect("/secrets");
+                });
+                
+            };
+        }
+    });
+    
 });
 
 app.get("/logout",function(req,res){
@@ -94,6 +189,8 @@ if (err) {
    });
     
 });
+
+
 
 app.post ("/login",function(req,res){
     const user = new User({
